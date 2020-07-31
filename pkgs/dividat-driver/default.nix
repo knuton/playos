bd9ba87f8c693174eedc6a930143c6e60940d730
Johannes Emerich <johannes@emerich.de>
2020-07-31 10:48:28 +0200

    Switch to driver for Senso Flex prototype 2

--- pkgs/dividat-driver/default.nix
+++ pkgs/dividat-driver/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl}:
 stdenv.mkDerivation rec {
   name = "dividat-driver-${version}";
-  version = "2.2.0-rc1-12-gc655566";
+  version = "2.2.0-rc1-14-g84c93ab";
   channel = "master";
 
-  src = ./dividat-driver-linux-amd64-2.2.0-rc1-12-gc655566;
+  src = ./dividat-driver-linux-amd64-2.2.0-rc1-14-g84c93ab;
 
   buildCommand = ''
     mkdir -p $out/bin
