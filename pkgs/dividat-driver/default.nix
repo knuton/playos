4380c6064e7eaa01eda0449ace4c0b1c4ecca462
Johannes Emerich <johannes@emerich.de>
2020-12-01 13:06:36 +0100

    Use experimental driver build

--- pkgs/dividat-driver/default.nix
+++ pkgs/dividat-driver/default.nix
@@ -1,10 +1,10 @@
 {stdenv, fetchurl}:
 stdenv.mkDerivation rec {
   name = "dividat-driver-${version}";
-  version = "2.2.0-rc1-18-g7ae8639";
+  version = "2.2.0-rc1-19-g7c7c59f";
   channel = "master";
 
-  src = ./dividat-driver-linux-amd64-2.2.0-rc1-18-g7ae8639;
+  src = ./dividat-driver-linux-amd64-2.2.0-rc1-19-g7c7c59f;
 
   buildCommand = ''
     mkdir -p $out/bin
