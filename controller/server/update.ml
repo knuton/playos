caa2267109cff136bd113ec90b1b7ac7e367379b
Johannes Emerich <johannes@dividat.com>
2024-08-25 11:40:22 +0200

    Remove double tracking of versions
    We perform checks using the parsed version number, and should not expect any discrepancy between a parseable version number and its serialization.

--- controller/server/update.ml
+++ controller/server/update.ml
@@ -12,13 +12,13 @@ let bundle_name =
 (** Type containing version information *)
 type version_info =
   {(* the latest available version *)
-    latest : (Semver.t [@sexp.opaque]) * string
+    latest : (Semver.t [@sexp.opaque])
 
   (* version of currently booted system *)
-  ; booted : (Semver.t [@sexp.opaque]) * string
+  ; booted : (Semver.t [@sexp.opaque])
 
   (* version of inactive system *)
-  ; inactive : (Semver.t [@sexp.opaque]) * string
+  ; inactive : (Semver.t [@sexp.opaque])
   }
 [@@deriving sexp]
 
@@ -32,7 +32,7 @@ let semver_of_string string =
     failwith
       (Format.sprintf "could not parse version (version string: %s)" string)
   | Some version ->
-    version, trimmed_string
+    version
 
 (** Get latest version available at [url] *)
 let get_latest_version ~proxy url =
@@ -126,15 +126,11 @@ let rec run ~connman ~update_url ~rauc ~set_state =
       | Ok version_info ->
 
         (* Compare latest available version to version booted. *)
-        let booted_version_compare = Semver.compare
-            (fst version_info.latest)
-            (fst version_info.booted) in
+        let booted_version_compare = Semver.compare version_info.latest version_info.booted in
         let booted_up_to_date = booted_version_compare = 0 in
 
         (* Compare latest available version to version on inactive system partition. *)
-        let inactive_version_compare = Semver.compare
-            (fst version_info.latest)
-            (fst version_info.inactive) in
+        let inactive_version_compare = Semver.compare version_info.latest version_info.inactive in
         let inactive_up_to_date = inactive_version_compare = 0 in
         let inactive_update_available = inactive_version_compare > 0 in
 
@@ -158,7 +154,7 @@ let rec run ~connman ~update_url ~rauc ~set_state =
 
         else if inactive_update_available then
           (* Booted system is not up to date and there is an update available for inactive system. *)
-          Downloading (version_info.latest |> snd)
+          Downloading (Semver.to_string version_info.latest)
           |> set
 
         else
